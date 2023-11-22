9ccd6094050c26a4d361ff9509b528401cb252f9
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-11-22 17:47:31 -0300

    add entity creation tests

--- spec/system/entity_creation_spec.rb
+++ spec/system/entity_creation_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe 'Group creation', type: :system do
+RSpec.describe 'Entity creation', type: :system do
   let(:user) do
     User.create(name: 'example_user', email: 'user@example.com', password: 'password', id: 1)
   end
   let(:group) do
-    Group.new(name: 'apple', icon: 'icon', user_id: user.id)
+    Group.create(name: 'transport', icon: 'airplane.png', user_id: user.id)
   end
 
   before do
@@ -13,23 +13,25 @@ RSpec.describe 'Group creation', type: :system do
     sign_in user
   end
 
-  it 'allows a user to create a new group' do
+  it 'allows a user to create a new entity' do
     visit new_user_group_entity_path(user, group)
-    fill_in 'name', with: 'transport'
+    fill_in 'name', with: 'car ride'
     fill_in 'amount', with: '10'
-    choose(page.all("input[type='radio']").first[:id])
-    click_button 'Create Group'
+    check(page.all("input[type='checkbox']").first[:id])
+    click_button 'Create Entity'
   
     expect(page).to have_content('transport')
+    expect(page).to have_content('1')
   end
 
-  # it 'allows a user to create a new group' do
-  #   visit new_user_group_entity_path(user, group)
+  it 're renders the page when user did not input values' do
+    visit new_user_group_entity_path(user, group)
 
-  #   click_button 'Create Group'
+    click_button 'Create Entity'
   
-  #   expect(page).to have_content('2 errors prohibited this group from being saved:')
-  #   expect(page).to have_content('Name can\'t be blank')
-  #   expect(page).to have_content('Icon can\'t be blank')
-  # end
+    expect(page).to have_content('3 errors prohibited this entity from being saved:')
+    expect(page).to have_content('Name can\'t be blank')
+    expect(page).to have_content('Groups can\'t be blank')
+    expect(page).to have_content('Amount can\'t be blank')
+  end
 end
